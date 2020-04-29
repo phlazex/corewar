597e2b13d1a881b66566c44251efaa2b63097d27
Evgeniy Nalivaev <e.nalivaev@gmail.com>
2020-04-29 21:11:38 +0300

    Добавлено чтение строки файла построчно

--- src/asm/asm.c
+++ src/asm/asm.c
@@ -10,17 +10,17 @@ int main(int argc, char **argv)
 {
 	t_project *project;
 
-	ft_printf(CLR);
+//	ft_printf(CLR);
 	if (argc == 2)
 	{
 		ft_project_init(argv[1], &project);
-		ft_print_memory(project->name, project->end, project->comment, project->program);
-		ft_set_color(white + 2);
-		ft_printf(".name \"%s\"\n", project->name);
-		ft_printf(".comment \"%s\"\n\n", project->comment);
-		project->current = project->program;
-		while (project->current < project->end)
-			ft_disassemble(project);
+//		ft_print_memory(project->name, project->end, project->comment, project->program);
+//		ft_set_color(white + 2);
+//		ft_printf(".name \"%s\"\n", project->name);
+//		ft_printf(".comment \"%s\"\n\n", project->comment);
+//		project->current = project->program;
+//		while (project->current < project->end)
+//			ft_disassemble(project);
 	}
 	else
 		ft_usage();
--- src/asm/ft_project_init.c
+++ src/asm/ft_project_init.c
@@ -4,15 +4,67 @@
 #include "asm.h"
 #include "libft.h"
 #include "corewar.h"
+#include <ft_printf.h>
 
-static int ft_parse_file(t_mem *mem, t_project *project)
+//static char *ft_name_comment(char *str)
+//{
+//	char	*start;
+//	char	*end;
+//	char	*newstr;
+//
+//	if ((start = ft_strchr(str, '"')) && (end = ft_strchr(start + 1, '"')))
+//	{
+//		newstr = ft_strsub(start, 1, end - start - 1);
+//	}
+//	return (newstr);
+//}
+
+static void	ft_parse_current(t_project *project)
+{
+	if (project->data->current == project->data->head)
+	{
+		ft_printf("START");
+	}
+	else
+	{
+		ft_printf("NEXT");
+	}
+}
+
+static int	ft_parse_file(t_mem *mem, t_project *project)
 {
 	project->data = mem;
-	project->name = mem->head + 4;
-	project->prog_size =  project->name + PROG_NAME_LENGTH + 4;
-	project->comment = project->prog_size + 4;
-	project->program = project->comment + COMMENT_LENGTH + 4;
-	project->end = mem->end;
+	if ((mem->endl = ft_strchr(mem->head, '\n')))
+	{
+		mem->current = mem->head;
+//		ft_printf("%s|%zi", mem->end, mem->end);
+	}
+	while (mem->current < mem->end)
+	{
+		ft_parse_current(project);
+//		ft_printf("%s|", project->name);
+		if ((mem->endl = ft_strchr(mem->current, '\n')))
+		{
+			mem->current = mem->endl + 1;
+		}
+	}
+//	if ((project->name = ft_strstr(mem->head, NAME_CMD_STRING)) && (project->comment = ft_strstr(mem->head, COMMENT_CMD_STRING)))
+//	{
+//		if (mem->head == project->name || mem->head == project->comment)
+//		{
+//			if (!ft_strstr(project->name + 1, NAME_CMD_STRING) && !ft_strstr(project->comment + 1, COMMENT_CMD_STRING))
+//			{
+//				project->name = ft_name_comment(project->name);
+//				project->comment = ft_name_comment(project->comment);
+//				ft_printf("%s|%s|%s", mem->head, project->name, project->comment);
+//			}
+//		}
+//	}
+//	project->name = mem->head + 4;
+//	project->prog_size =  project->name + PROG_NAME_LENGTH + 4;
+//	project->comment = project->prog_size + 4;
+//	project->program = project->comment + COMMENT_LENGTH + 4;
+//	project->end = mem->end;
 	return 0;
 }
 
